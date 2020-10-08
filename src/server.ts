110adeb7969f1eef06eaa0a1c4f50c9cd75c6df8
Lucas <lucas.midian@gmail.com>
2020-10-07 21:28:01 -0300

    remove capture global errors

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,35 @@
 import express, { Request, Response, NextFunction } from 'express';
+/**
+ * pacote para capturar os erros,
+ * devido o uso de async
+ **/
+import 'express-async-errors';
+
 import routes from './routes';
 import './database';
 import uploadConfig from './config/upload';
 import AppError from './error/AppError';
+
 const app = express();
 
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        status: 'error',
-        message: err.message,
-      });
-    }
-
-    console.error(err);
-
-    return response.status(500).json({
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
       status: 'error',
-      message: 'Internal server error',
+      message: err.message,
     });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
 });
 
 app.listen(3333, () => {
